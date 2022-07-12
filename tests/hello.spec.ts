c338422b922ca33217e13fdbd7162486be9b151b
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-07-12 15:40:23 -0400

    set up ts

---
+++ tests/hello.spec.ts
@@ -0,0 +1,20 @@
+import { assert } from "chai"
+// import Node from "../data_structures/Node.js"
+
+describe("Testing the Node class", () => {
+  // let node
+  // beforeEach(() => {
+  //   node = new Node(1)
+  // })
+  // it("should get the value of the node as 1", () => {
+  //   assert.equal(node.getValue(), 1)
+  // })
+
+  // it("should set the value of the node to 2", () => {
+  //   node.setValue(2)
+  //   assert.equal(node.getValue(), 2)
+  // })
+  it("should pass the test", () => {
+    assert.equal(1, 1)
+  })
+})
